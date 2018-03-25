fd3b9e700d58912887de52b6e0fc46594a01b569
johntiger1 <john.person1@gmail.com>
2018-03-24 20:01:36 -0400

    demonstrate express func

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
+var express        =        require("express");
+var bodyParser     =        require("body-parser");
+var app            =        express();
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/handle',function(request,response){
+    response.send("hello, im am in handle");
+});
+
+app.get('/', function(request,response){
+    response.send("hello, im am in general, the root");
+});
 
 
 
-var http = require('http');
-var qs = require('querystring');
+app.listen(3000,
 
-
-
-function req_res_listener(req, res)
-{
-    res.writeHead(200,{'Content-type':'text/json'});
-
-    if (req.method === 'POST') {
-        var body = '';
-
-        req.on('data', function (data) {
-            body += data;
-        });
-
-        req.on('end', function () {
-            var post = qs.parse(body);
-            console.log(post);
-            console.log("this is the value" + post["abc"]);
-            console.log("what about this way?" + post.abc);
-
-            // use post['blah'], etc.
-        });
-
-    }
-    res.end("this is a message from the backend");
-
-
-//    there is no: "easy" way to access the request params!
-
-}
-
-http.createServer(req_res_listener
-
-).listen(1337, '127.0.0.1');
-console.log("Server now up and running! ");
-
+    () => console.log("listening via express"));
